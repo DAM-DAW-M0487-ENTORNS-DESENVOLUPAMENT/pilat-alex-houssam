dc920d09022b18c44a91d7c7c6f19690fbc95095
MolotoveDev <alex.carrasco005@gmail.com>
2025-03-31 12:31:32 +0200

    Començada clase compilador

--- Compilador.cs
+++ Compilador.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+<<<<<<< HEAD
 using PILA;
 
 namespace PilaT
@@ -52,6 +53,51 @@ namespace PilaT
             }
             if(!llista.IsEmpty) { resultat = false; } // si al acabar encara queda alguna cosa en la pila vol dir que esta malament.
             return resultat;
+=======
+
+namespace PilaT
+{
+    public class Compilador :Stack<char>
+    {
+        public Compilador()
+        {
+        }
+        bool valid = false;
+        public bool Validar(string expressio)
+        {
+            foreach (char c in expressio)
+            {
+                if (c == Convert.ToChar("(") || c == Convert.ToChar("[") || c == Convert.ToChar("{"))
+                {
+                    this.Push(c);
+                }
+                else if (c == Convert.ToChar(")") || c == Convert.ToChar("]") || c == Convert.ToChar("}"))
+                {
+                    if (this.Count == 0)
+                    {
+                        valid = false;
+                        throw new ArgumentException("Error en la expresion");
+                    }
+                    char temp = this.Pop();
+                    if (c == Convert.ToChar(")") && temp != Convert.ToChar("("))
+                    {
+                        valid = false;
+                        throw new ArgumentException("Error en la expresion");
+                    }
+                    if (c == Convert.ToChar("]") && temp != Convert.ToChar("["))
+                    {
+                        valid = false;
+                        throw new ArgumentException("Error en la expresion");
+                    }
+                    if (c == Convert.ToChar("}") && temp != Convert.ToChar("{"))
+                    {
+                        valid = false;
+                        throw new ArgumentException("Error en la expresion");
+                    }
+                }
+            }
+            return valid;
+>>>>>>> 5659bec (Començada clase compilador)
         }
     }
 }
